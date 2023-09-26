4d41f5d4654a2202f02071d8ef48cd03c24dee09
iamlydial <lydia.lavecchia@gmail.com>
2023-09-26 11:29:15 +0200

    added new item form, views and base html link

--- tailbar/item/views.py
+++ tailbar/item/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from .models import Item
+from .forms import NewItemForm
+
 
 # Create your views here.
 def detail(request, pk):
@@ -10,3 +13,12 @@ def detail(request, pk):
         'item': item,
         'related_items' : relates_items
     })
+
+def new(request):
+    form = NewItemForm()
+    return render(request, 'item/form.html', {
+        'form': form, 
+        'title': 'New Item'
+    })
+
+
